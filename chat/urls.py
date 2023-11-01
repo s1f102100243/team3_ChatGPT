cfab13ba5c917c41eb0299ce7a1a71f329c88300
s1f102101794 <85730873+s1f102101794@users.noreply.github.com>
2023-11-01 16:10:01 +0900

    フィードバック機能（仮）
    あとからいろいろ足します

--- chat/urls.py
+++ chat/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('<int:article_id>/', views.detail, name='detail'),
+	path('<int:article_id>/delete', views.delete, name='delete'),
+    path('<int:article_id>/update', views.update, name='update'),
+    path('<int:article_id>/like', views.know, name='like'),
+    #path("<str:room_name>/", views.room, name="room"),
 ]
